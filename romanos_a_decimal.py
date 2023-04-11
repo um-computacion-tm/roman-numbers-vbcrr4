9cb9f445304ebcda365b80052b9dda71aee917b3
v.becerra <v.becerra@alumno.um.edu.ar>
2023-04-10 23:33:45 -0300

    cambio de nombre de funcion

--- romanos_a_decimal.py
+++ romanos_a_decimal.py
@@ -1,6 +1,6 @@
-def decimal_to_roman(romano):
+def roman_to_decimal(romano):
     d_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C' : 100, 'D': 500, 'M': 1000}
-    d_decimal= [d_num[var] for var in romano] #char=variable temporal para iterar
+    d_decimal= [d_num[var] for var in romano] #cvariable temporal para iterar
     total = 0
     for i in range(len(d_decimal)):
         if i == len(d_decimal) -1 or d_decimal[i] >= d_decimal[i+1]:
